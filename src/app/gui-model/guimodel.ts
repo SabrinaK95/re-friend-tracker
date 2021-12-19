c26aa35fc16f5274b741252b4ef74c864661072d
SabrinaK95 <sabsikramer@googlemail.com>
2021-12-19 09:32:48 +0000

    change name

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Sabrinas Friend Tracker",
+            "title": "Sabrina Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
