4ea78ebb3628b673f75e29b94314ef12cf9a9570
SabrinaK95 <sabsikramer@googlemail.com>
2021-12-19 09:53:19 +0000

    add groupspage

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -27,6 +27,13 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+                        { 
+                            "id": "nickname", 
+                            "type": "text", 
+                            "name": "Nickname", 
+                            "width": 2, 
+                            "required": true 
+                        },
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -138,6 +145,13 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+                        { 
+                            "type": "button", 
+                            "name": "Groups", 
+                            "icon": "fa-weixin", 
+                            "color": "wisteria", 
+                            "page": "groupspage", 
+                        },
                     ]
                 },
                 {
@@ -195,6 +209,24 @@ export class GuiModel {
                         },
                     ]
                 },
+
+                { 
+                    "id": "groupspage", 
+                    "elementList": [ 
+                        { 
+                            "type": "backbutton", 
+                        }, 
+                        { 
+                            "type": "newButton", 
+                            "name": "NewGroup", 
+                            "icon": "fa-weixin", 
+                            "color": "green", 
+                            "form": { 
+                                "form": "GroupForm" 
+                            } 
+                        } 
+                    ] 
+                }
             ]
         }
     };
